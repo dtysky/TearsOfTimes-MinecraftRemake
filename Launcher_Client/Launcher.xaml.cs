a00ddbd3b13ec36d5f7190b31fe04fb83d6257ba
npk48 <nyanyacatz@gmail.com>
2015-08-17 01:05:50 +0200

    *add Logger *add Common Helper in structure

--- Launcher_Client/Launcher.xaml.cs
+++ Launcher_Client/Launcher.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,32 @@ namespace Minecraft.Launcher
         public MainWindow()
         {
             InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            Helper.Logger Log = Helper.Logger.Instance;
+            Console.SetOut(Log);
+            DataContext = Log.Content;
+            Loaded += new RoutedEventHandler(delegate (object sender, RoutedEventArgs e)
+            {
+                InputBlock.KeyDown += new KeyEventHandler(delegate (object _sender, KeyEventArgs _e)
+                {
+                    if (_e.Key == Key.Enter)
+                    {
+                        Log.RunCommand(InputBlock.Text);
+                        InputBlock.Text = "";
+                        InputBlock.Focus();
+                    }
+                });
+                Log.Updated += new Helper.Logger.UpdatedEventHandler(delegate (string value)
+                {
+                    Scroller.ScrollToBottom();
+                    if (Log.Content.Output.Count > 5)
+                        Log.Content.Output.RemoveAt(0);
+                });
+            }); 
         }
 
         private void Drag(object sender, MouseButtonEventArgs e)
@@ -42,7 +69,7 @@ namespace Minecraft.Launcher
 
         private void Button_Launch_Click(object sender, RoutedEventArgs e)
         {
-
+            Console.WriteLine("test");
         }
 
         private void Button_Settings_Click(object sender, RoutedEventArgs e)
@@ -57,12 +84,14 @@ namespace Minecraft.Launcher
         {
             Scroller.Visibility = Visibility.Visible;
             InputBlock.Visibility = Visibility.Visible;
+            Helper.Logger.Instance.Enable = true;
         }
 
         private void CheckBox_Log_Unchecked(object sender, RoutedEventArgs e)
         {
             Scroller.Visibility = Visibility.Hidden;
             InputBlock.Visibility = Visibility.Hidden;
+            Helper.Logger.Instance.Enable = false;
         }
     }
 }
---
+++ Launcher_Client/Logger.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Minecraft.Helper
+{
+    class Logger : TextWriter
+    {
+        public static Logger Instance = new Logger();
+        public Logger_Content Content = new Logger_Content();
+
+        public bool Enable = true;
+
+        private Logger()
+        {
+         
+        }
+
+        public override void WriteLine(string Value)
+        {
+            if (!Enable) return;
+            Content.Output.Add(Value);
+            Updated(Value);
+        }
+
+        public void RunCommand(string Command)
+        {
+            WriteLine(Command);
+            ExecuteCommand(Command);
+        }
+
+        public override Encoding Encoding
+        {
+            get { return Encoding.Default; }
+        }
+
+        public delegate void UpdatedEventHandler(string Value);
+
+        public event UpdatedEventHandler Updated = delegate { };
+
+        public delegate void ExecuteCommandEventHandler(string Value);
+
+        public event ExecuteCommandEventHandler ExecuteCommand = delegate { };
+           
+    }
+
+    class Logger_Content : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (null != PropertyChanged)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        string _Input = string.Empty;
+        public string Input
+        {
+            get
+            {
+                return _Input;
+            }
+            set
+            {
+                _Input = value;
+                OnPropertyChanged("Input");
+            }
+        }
+        ObservableCollection<string> _Output = new ObservableCollection<string>() { };
+
+        public ObservableCollection<string> Output
+        {
+            get
+            {
+                return _Output;
+            }
+            set
+            {
+                _Output = value;
+                OnPropertyChanged("Output");
+            }
+        }
+
+        
+
+        
+    }
+}
